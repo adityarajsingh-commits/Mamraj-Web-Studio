fe20e399a16a55f1f0183283b5df1c1198cc8c8b
adityarajsingh-commits <adityarajsingh@mamrajwebstudio.com>
2026-07-25 15:23:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from
 
 // Firebase Configuration Details
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
